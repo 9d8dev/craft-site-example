b79b76e75a8116e8cd0da8bbbc51fee51bca5677
Bridger Tower <bridgertower@gmail.com>
2023-12-15 14:29:43 -0700

    layout, section, container

---
+++ components/craft.tsx
@@ -0,0 +1,48 @@
+// Layout
+
+type LayoutProps = {
+	children: React.ReactNode;
+	className?: string;
+};
+
+const Layout = ({ children, className }: LayoutProps) => {
+	return (
+		<html lang="en" className="p-6 bg-secondary-100 dark:bg-secondary-900">
+			<body className={className}>{children}</body>
+		</html>
+	);
+};
+
+// Section
+
+type SectionProps = {
+	children: React.ReactNode;
+	className?: string;
+	style?: React.CSSProperties;
+	id?: string;
+};
+
+const Section = ({ children, className, style, id }: SectionProps) => {
+	return (
+		<section className={className} style={style} id={id}>
+			{children}
+		</section>
+	);
+};
+
+type ContainerProps = {
+	children: React.ReactNode;
+	className?: string;
+	style?: React.CSSProperties;
+	id?: string;
+};
+
+const Container = ({ children, className, style, id }: ContainerProps) => {
+	return (
+		<div className={className} style={style} id={id}>
+			{children}
+		</div>
+	);
+};
+
+export { Layout };
